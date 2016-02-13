c1f4675d838b91de3204712d9e86347520012500
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2016-02-13 23:54:53 +0000

    physics working

--- gravity-ball/gravity-ball/GameScene.swift
+++ gravity-ball/gravity-ball/GameScene.swift
@@ -9,13 +9,17 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var ball:SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-       
+       ball = self.childNodeWithName("ball") as! SKSpriteNode
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+        ball.physicsBody?.applyImpulse(CGVector(dx: 0,dy: 100))
       
     }
    
